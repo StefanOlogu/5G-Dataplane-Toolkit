410747d3b7e5fa7d2a4fa3cbca3b6435ac1297cf
Stefan Ologu <alexologu2005@gmail.com>
2026-09-14 02:16:10 +0300

    Added error tracing and graceful shutdowns by closing the connection if one direction stops and the other waits infinitely

--- src/bin/proxy.rs
+++ src/bin/proxy.rs
@@ -1,29 +1,61 @@
 use tokio::net::{TcpListener, TcpStream};
+use tokio::select;
+use tracing::{info, error, warn};
+use tokio::signal::ctrl_c;
 
 #[tokio::main]
 async fn main()-> std::io::Result<()> {
+    tracing_subscriber::fmt::init();
     let listener = TcpListener::bind("127.0.0.1:6060").await?;
+    info!("Proxy listening on 127.0.0.1:6060");
 
     loop{
-        let (mut stream, addr) = listener.accept().await?;
-        tokio::spawn(async move {
-            let mut upstream = match TcpStream::connect("127.0.0.1:5050").await {
-                Ok(s) => s,
-                Err(e) => {
-                    println!("upstream dial failed: {e}");
-                    return;
-                }
-            };
-            println!("client connected: {:?}", addr);
+        select! {
+            result = listener.accept() => {
+                let (mut stream, addr) = match result{
+                    Ok(a) => a,
+                    Err(e) => {
+                        error!("Failed to accept connection: {}", e);
+                        continue;
+                    }
+                };
+                tokio::spawn(async move {
+                    let mut upstream = match TcpStream::connect("127.0.0.1:5050").await {
+                        Ok(s) => s,
+                        Err(e) => {
+                            error!("Failed to dial upstream: {e}");
+                            return;
+                        }
+                    };
+                    info!("Client connected: {:?}", addr);
 
-            let (mut client_read, mut client_write) = stream.split();
-            let (mut upstream_read, mut upstream_write) = upstream.split();
-            let client_upstream = tokio::io::copy(&mut client_read, &mut upstream_write);
-            let upstream_client = tokio::io::copy(&mut upstream_read, &mut client_write);
+                    match tokio::io::copy_bidirectional(&mut stream, &mut upstream).await{
+                        Ok((to_upstream,to_client)) => {
+                            info!("Connection {:?} closed : {to_upstream:?} bytes up, {to_client:?} bytes down",addr);
+                        }
+                        Err(e)=>{
+                            warn!("Connection {:?} error: {e}",addr);
+                            return;
+                        }
+                    };
 
-            if let Err(e) = tokio::try_join!(client_upstream, upstream_client) {
-                println!("client disconnected: {e}");
+                    // let (mut client_read, mut client_write) = stream.split();
+                    // let (mut upstream_read, mut upstream_write) = upstream.split();
+                    // let client_upstream = tokio::io::copy(&mut client_read, &mut upstream_write);
+                    // let upstream_client = tokio::io::copy(&mut upstream_read, &mut client_write);
+                    //
+                    // if let Err(e) = tokio::try_join!(client_upstream, upstream_client) {
+                    //     warn!("Client or Proxy disconnected: {e}");
+                    // }
+
+
+                });
             }
-        });
+            _ = ctrl_c() => {
+                info!("Shutdown signal received");
+                break;
+            }
+        }
     }
+    Ok(())
 }
