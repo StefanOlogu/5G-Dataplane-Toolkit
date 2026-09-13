5bb995333c11849b74d8cdde9796102b1c5ccf39
Stefan Ologu <alexologu2005@gmail.com>
2026-09-14 00:24:43 +0300

    Created a TCP concurrent echo server

---
+++ src/bin/echo.rs
@@ -0,0 +1,37 @@
+use tokio::net::TcpListener;
+use tokio::io::AsyncReadExt;
+use tokio::io::AsyncWriteExt;
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:5050").await?;
+    loop{
+        let (mut socket, addr) = listener.accept().await?;
+        tokio::spawn(async move {
+            println!("Accepted connection from {:?}", addr);
+            loop {
+                let mut buf = [0u8; 1024];
+                let n = match socket.read(&mut buf).await{
+                    Ok(n) => n,
+
+                    Err(e) => {
+                        println!("failed to read from socket; err = {:?}", e);
+                        break;
+                    }
+                };
+
+                if n == 0 {
+                    break;
+                }
+                else{
+                    match socket.write_all(&buf[..n]).await{
+                        Ok(_) => {},
+                        Err(e) => {
+                            println!("failed to write to socket; err = {:?}", e);
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
